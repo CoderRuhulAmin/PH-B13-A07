15c569fc8bc1d1902f8c734b9535f7bc0de647a1
CoderRuhulAmin <coderruhulamin@gmail.com>
2026-04-15 02:21:06 +0600

    solved some responsive issue.

--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -11,33 +11,33 @@ const FriendList = () => {
     return (
         <section className='px-4 py-12'>
             <div className='container mx-auto'>
-                <div className='flex justify-between items-center'>
+                <div className='flex flex-wrap justify-between items-center'>
                     <h2 className='text-2xl font-bold mb-5'>Your Friends</h2>
-                    <div className="flex gap-3 mb-6">
+                    <div className="flex flex-wrap gap-3 mb-6">
                         <button
                             onClick={() => handleFilterChange('all')}
-                            className={`btn ${statusFilter === 'all' ? 'btn-primary' : 'btn-outline'}`}
+                            className={`btn btn-sm ${statusFilter === 'all' ? 'btn-primary' : 'btn-outline'}`}
                         >
                             All
                         </button>
 
                         <button
                             onClick={() => handleFilterChange('on-track')}
-                            className={`btn ${statusFilter === 'on-track' ? 'btn-success' : 'btn-outline'}`}
+                            className={`btn btn-sm ${statusFilter === 'on-track' ? 'btn-success' : 'btn-outline'}`}
                         >
                             On Track
                         </button>
 
                         <button
                             onClick={() => handleFilterChange('almost due')}
-                            className={`btn ${statusFilter === 'almost due' ? 'btn-warning' : 'btn-outline'}`}
+                            className={`btn btn-sm ${statusFilter === 'almost due' ? 'btn-warning' : 'btn-outline'}`}
                         >
                             Almost Due
                         </button>
 
                         <button
                             onClick={() => handleFilterChange('overdue')}
-                            className={`btn ${statusFilter === 'overdue' ? 'btn-error' : 'btn-outline'}`}
+                            className={`btn btn-sm ${statusFilter === 'overdue' ? 'btn-error' : 'btn-outline'}`}
                         >
                             Overdue
                         </button>
@@ -52,7 +52,7 @@ const FriendList = () => {
                     )
 
                 }
-                <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10'>
+                <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-10'>
                     {
                         filteredFriends.map(friend => <FriendCard key={friend.id} friend={friend} />)
                     }

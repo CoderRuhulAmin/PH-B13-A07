b067f8f087a7168b18ebeadab6ba6b51b75bfc24
CoderRuhulAmin <coderruhulamin@gmail.com>
2026-04-14 22:07:59 +0600

    Also added data loading spinner .

--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -45,9 +45,11 @@ const FriendList = () => {
                 </div>
                 <div className="flex justify-center w-full py-10">
                     {
+                        
                         loading && (
                             <span className="loading loading-spinner loading-lg text-success "></span>
                         )
+
                     }
                 </div>
                 <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10'>

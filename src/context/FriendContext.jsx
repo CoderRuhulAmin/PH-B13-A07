f5f671330ea160650e41afbd713904330e7f0aca
CoderRuhulAmin <coderruhulamin@gmail.com>
2026-04-14 17:55:04 +0600

    Netlify reloading problem solved

---
+++ src/context/FriendContext.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createContext } from "react";
+
+export const FriendContext = createContext();
+
+
+const FriendProvider = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default FriendProvider;
